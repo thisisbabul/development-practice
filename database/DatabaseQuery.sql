dae9e3d45bf9c54ddb2ee451ae93cd6142ae877c
babul3210 <babul.cu.cse35@gmail.com>
2018-02-17 02:30:04 +0600

    added database query

---
+++ database/DatabaseQuery.sql
@@ -0,0 +1,17 @@
+USE `test`;
+DROP function IF EXISTS `income_function`;
+
+DELIMITER $$
+USE `test`$$
+CREATE DEFINER=`root`@`localhost` FUNCTION `income_function`(value INTEGER) RETURNS int(11)
+BEGIN
+	declare income INT;
+    SET income = 0;
+    
+    label1: while income<=300 do
+		SET income = income + value;
+	end while label1;
+	RETURN income;
+END$$
+
+DELIMITER ;
